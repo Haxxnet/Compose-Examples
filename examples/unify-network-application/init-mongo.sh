f9c1f6523a33ee8fe95fe0d245453f8a13f315ff
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2026-02-22 15:49:08 +0100

    Create init-mongo.sh

---
+++ examples/unify-network-application/init-mongo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if which mongosh > /dev/null 2>&1; then
+  mongo_init_bin='mongosh'
+else
+  mongo_init_bin='mongo'
+fi
+"${mongo_init_bin}" <<EOF
+use ${MONGO_AUTHSOURCE}
+db.auth("${MONGO_INITDB_ROOT_USERNAME}", "${MONGO_INITDB_ROOT_PASSWORD}")
+db.createUser({
+  user: "${MONGO_USER}",
+  pwd: "${MONGO_PASS}",
+  roles: [
+    { db: "${MONGO_DBNAME}", role: "dbOwner" },
+    { db: "${MONGO_DBNAME}_stat", role: "dbOwner" },
+    { db: "${MONGO_DBNAME}_audit", role: "dbOwner" }
+  ]
+})
+EOF
